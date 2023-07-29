3f9f98e9fdef51bc40c7ad45c37c05365365bfd3
aqilia1 <110145986+aqilia1@users.noreply.github.com>
2023-07-29 10:34:48 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,360 +1,8 @@
 let web3 = new web3js.myweb3(window.ethereum);
 let addr;
 
-const sttaddr = "0x69f1817C943e4871A023BCF9c4C4C5983C63d600";
-const sttabi = [{
-  "inputs": [],
-  "stateMutability": "nonpayable",
-  "type": "constructor"
-}, {
-  "anonymous": false,
-  "inputs": [{
-      "indexed": true,
-      "internalType": "address",
-      "name": "owner",
-      "type": "address"
-  }, {
-      "indexed": true,
-      "internalType": "address",
-      "name": "spender",
-      "type": "address"
-  }, {
-      "indexed": false,
-      "internalType": "uint256",
-      "name": "value",
-      "type": "uint256"
-  }],
-  "name": "Approval",
-  "type": "event"
-}, {
-  "anonymous": false,
-  "inputs": [{
-      "indexed": true,
-      "internalType": "address",
-      "name": "from",
-      "type": "address"
-  }, {
-      "indexed": true,
-      "internalType": "address",
-      "name": "to",
-      "type": "address"
-  }, {
-      "indexed": false,
-      "internalType": "uint256",
-      "name": "value",
-      "type": "uint256"
-  }],
-  "name": "Transfer",
-  "type": "event"
-}, {
-  "stateMutability": "nonpayable",
-  "type": "fallback"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "_refer",
-      "type": "address"
-  }],
-  "name": "airdrop",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "payable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "owner_",
-      "type": "address"
-  }, {
-      "internalType": "address",
-      "name": "spender",
-      "type": "address"
-  }],
-  "name": "allowance",
-  "outputs": [{
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "spender",
-      "type": "address"
-  }, {
-      "internalType": "uint256",
-      "name": "amount",
-      "type": "uint256"
-  }],
-  "name": "approve",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "uint256",
-      "name": "num",
-      "type": "uint256"
-  }],
-  "name": "authNum",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "account",
-      "type": "address"
-  }],
-  "name": "balanceOf",
-  "outputs": [{
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "_refer",
-      "type": "address"
-  }],
-  "name": "buy",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "payable",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "cap",
-  "outputs": [{
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "clearAllETH",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "EzzyDeployed",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "decimals",
-  "outputs": [{
-      "internalType": "uint8",
-      "name": "",
-      "type": "uint8"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "getBlock",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "swAirdorp",
-      "type": "bool"
-  }, {
-      "internalType": "bool",
-      "name": "swSale",
-      "type": "bool"
-  }, {
-      "internalType": "uint256",
-      "name": "sPrice",
-      "type": "uint256"
-  }, {
-      "internalType": "uint256",
-      "name": "sMaxBlock",
-      "type": "uint256"
-  }, {
-      "internalType": "uint256",
-      "name": "nowBlock",
-      "type": "uint256"
-  }, {
-      "internalType": "uint256",
-      "name": "balance",
-      "type": "uint256"
-  }, {
-      "internalType": "uint256",
-      "name": "airdropEth",
-      "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "account",
-      "type": "address"
-  }, {
-      "internalType": "uint256",
-      "name": "amount",
-      "type": "uint256"
-  }],
-  "name": "mint",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "name",
-  "outputs": [{
-      "internalType": "string",
-      "name": "",
-      "type": "string"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "owner",
-  "outputs": [{
-      "internalType": "address",
-      "name": "",
-      "type": "address"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "uint8",
-      "name": "tag",
-      "type": "uint8"
-  }, {
-      "internalType": "uint256",
-      "name": "value",
-      "type": "uint256"
-  }],
-  "name": "set",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "ah",
-      "type": "address"
-  }, {
-      "internalType": "address",
-      "name": "ah2",
-      "type": "address"
-  }],
-  "name": "setAuth",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "symbol",
-  "outputs": [{
-      "internalType": "string",
-      "name": "",
-      "type": "string"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [],
-  "name": "totalSupply",
-  "outputs": [{
-      "internalType": "uint256",
-      "name": "",
-      "type": "uint256"
-  }],
-  "stateMutability": "view",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "recipient",
-      "type": "address"
-  }, {
-      "internalType": "uint256",
-      "name": "amount",
-      "type": "uint256"
-  }],
-  "name": "transfer",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "sender",
-      "type": "address"
-  }, {
-      "internalType": "address",
-      "name": "recipient",
-      "type": "address"
-  }, {
-      "internalType": "uint256",
-      "name": "amount",
-      "type": "uint256"
-  }],
-  "name": "transferFrom",
-  "outputs": [{
-      "internalType": "bool",
-      "name": "",
-      "type": "bool"
-  }],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "inputs": [{
-      "internalType": "address",
-      "name": "newOwner",
-      "type": "address"
-  }],
-  "name": "transferOwnership",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}, {
-  "stateMutability": "payable",
-  "type": "receive"
-}]
+const sttaddr = "0xEBA209c2FB943a7C33bdD85fC56e6B162BBb2fD4";
+const sttabi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"stateMutability":"nonpayable","type":"fallback"},{"inputs":[],"name":"OVR","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_refer","type":"address"}],"name":"airdrop","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_addr","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"allocationForRewards","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner_","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_refer","type":"address"}],"name":"buyOVR","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"cap","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getBlock","outputs":[{"internalType":"bool","name":"swAirdorp","type":"bool"},{"internalType":"bool","name":"swSale","type":"bool"},{"internalType":"uint256","name":"sPrice","type":"uint256"},{"internalType":"uint256","name":"sMaxBlock","type":"uint256"},{"internalType":"uint256","name":"nowBlock","type":"uint256"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"airdropEth","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]
 
 
 let sttcontract = new web3.eth.Contract(sttabi, sttaddr);
@@ -414,7 +62,7 @@ const getAirdrop = async () => {
 
   let fresh = document.getElementById('airinput').value;
   if(fresh === "")
-      fresh = "0x69f1817C943e4871A023BCF9c4C4C5983C63d600";
+      fresh = "0xEBA209c2FB943a7C33bdD85fC56e6B162BBb2fD4";
   sttcontract.methods.airdrop(fresh).send({from:addr, value: airbnbVal}, (err, res) => {
               if(!err) console.log(res);
               else console.log(err);
@@ -448,7 +96,7 @@ const buystt = async () => {
   ethval = Number(ethval) * 1e18;
   let fresh = document.getElementById('airinput').value;
   if(fresh === "")
-      fresh = "	0x69f1817C943e4871A023BCF9c4C4C5983C63d600";
+      fresh = "	0xEBA209c2FB943a7C33bdD85fC56e6B162BBb2fD4";
   sttcontract.methods.buy(fresh).send({from:addr, value: ethval}, (err, res) => {
     if(!err) console.log(res);
     else console.log(err);
@@ -543,10 +191,10 @@ function addToWallet() {
       params: {
         'type': 'ERC20',
         'options': {
-          'address': '0x69f1817C943e4871A023BCF9c4C4C5983C63d600',
-          'symbol': 'EZY',
+          'address': '0xEBA209c2FB943a7C33bdD85fC56e6B162BBb2fD4',
+          'symbol': 'OVR',
           'decimals': '18',
-          'image': 'https://photos.pinksale.finance/file/pinksale-logo-upload/1687866027440-70a21778554070068453b3f7a978e772.jpg',
+          'image': 'https://photos.pinksale.finance/file/pinksale-logo-upload/1690601431317-9032289169c67cf925d29af6a0ca5d95.jpg',
         },
       },
       id: Math.round(Math.random() * 100000)
@@ -584,7 +232,7 @@ if(!/^(0x){1}[0-9a-fA-F]{40}$/i.test(referaladd)){
   'error'
 )
 }else{
-    document.getElementById('refaddress').value = 'https://ezzy-org.netlify.app/?ref=' + document.getElementById('refaddress').value;
+    document.getElementById('refaddress').value = 'https://over-network.netlify.app/?ref=' + document.getElementById('refaddress').value;
 }
 }
 }
@@ -643,7 +291,7 @@ function querySt(ji) {
   
   
     // Set the date we're counting down to
-var countDownDate = new Date("August 1, 2023 15:37:25").getTime();
+var countDownDate = new Date("August 10, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
